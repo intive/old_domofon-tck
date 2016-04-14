f1cc92c75f29637a0c89912b17301e66ba6a97da
Marcin Matuszak <matuszak.marcin@gmail.com>
2016-04-14 12:42:17 +0200

    Add remove contact test

---
+++ tck/src/main/scala/domofon/tck/RemoveContactItemTest.scala
@@ -0,0 +1,26 @@
+package domofon.tck
+
+import akka.http.scaladsl.model.StatusCodes
+
+trait RemoveContactItemTest extends BaseTckTest {
+
+  describe("DELETE /contacts/{id}") {
+
+    it("When contact doesn't exist 404 is returned") {
+
+      Delete(s"/contacts/$nonExistentUuid") ~> domofonRoute ~> check {
+        status shouldBe StatusCodes.NotFound
+      }
+    }
+
+    it("Removed contact when one exists") {
+      val uuid = postContactRequest()
+
+      Delete(s"/contacts/$uuid") ~> domofonRoute ~> check {
+        status shouldBe StatusCodes.OK
+      }
+    }
+  }
+}
+
+
