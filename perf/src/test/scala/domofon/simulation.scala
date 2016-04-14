b6b2abb7f41c14709adb1d7eb235c48bdd81692b
Marcin Matuszak <matuszak.marcin@gmail.com>
2016-04-14 17:12:46 +0200

    Define scenario

--- perf/src/test/scala/domofon/simulation.scala
+++ perf/src/test/scala/domofon/simulation.scala
@@ -2,16 +2,19 @@ package domofon
 
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
-
 import io.gatling.core.scenario.Simulation
+
+import concurrent.duration._
+import scala.language.postfixOps
 
 trait Config {
   lazy val uri = System.getenv("DEST_URI")
 }
 
 object SimulationConfig extends Config {
+
   val httpConf = http.baseURL(s"http://$uri")
-  val injection = atOnceUsers(1)
+  val injection = atOnceUsers(1000)
 }
 
 class ContactSimulation
@@ -24,5 +27,10 @@ class ContactSimulation
     `update-contact-scenario`.
       inject(SimulationConfig.injection).
       protocols(SimulationConfig.httpConf)
-  )
+  ).throttle(
+      reachRps(100) in (10 seconds),
+      holdFor(1 minute),
+      jumpToRps(500),
+      holdFor(10 minutes)
+    )
 }
