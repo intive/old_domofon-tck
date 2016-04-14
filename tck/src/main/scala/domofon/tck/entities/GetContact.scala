f276266596d08cb16756ca760722375af796fc18
Lukasz Stefaniak <lustefaniak@gmail.com>
2016-04-14 15:37:30 +0200

    Remove dead code in tck

--- tck/src/main/scala/domofon/tck/entities/GetContact.scala
+++ tck/src/main/scala/domofon/tck/entities/GetContact.scala
@@ -1,6 +1,5 @@
 package domofon.tck.entities
 
-import java.time.LocalDate
 import java.util.UUID
 
 case class GetContact(
@@ -17,23 +16,3 @@ case class GetContact(
   deputy: Option[Deputy] = None
 )
 
-case object GetContact {
-
-  def from(id: UUID, req: PostContact): GetContact = {
-    GetContact(
-      id,
-      req.name,
-      req.company,
-      req.notifyEmail,
-      req.phone,
-      req.adminEmail.getOrElse(req.notifyEmail),
-      false,
-      "",
-      req.fromDate.getOrElse(LocalDate.now()),
-      req.tillDate,
-      None
-    )
-
-  }
-
-}
