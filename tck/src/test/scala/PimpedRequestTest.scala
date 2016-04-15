349bf6c2f413b3efa012c0ff73ed7db2eef5bad6
Lukasz Stefaniak <lustefaniak@gmail.com>
2016-04-15 18:09:19 +0200

    Add some tests to verify if on error BaseTckTest prints Request and Response

---
+++ tck/src/test/scala/PimpedRequestTest.scala
@@ -0,0 +1,19 @@
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
+import domofon.tck.BaseTckTest
+
+class PimpedRequestTest extends BaseTckTest {
+  override def domofonRoute: Route = Route.seal(reject())
+
+  describe("PimpedRequest") {
+    it("Should print detailed error in case of failure (pending is OK)") {
+      pendingUntilFixed {
+        Get("/") ~~> {
+          status shouldBe StatusCodes.OK
+        }
+      }
+    }
+  }
+
+}
