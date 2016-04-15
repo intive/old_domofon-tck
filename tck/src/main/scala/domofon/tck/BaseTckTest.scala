cef49e442640917ab985a297e0689bf22b7cf588
Lukasz Stefaniak <lustefaniak@gmail.com>
2016-04-15 17:25:53 +0200

    Helper methods to simplify calling domofonRoute and asserting response with some debug on error

--- tck/src/main/scala/domofon/tck/BaseTckTest.scala
+++ tck/src/main/scala/domofon/tck/BaseTckTest.scala
@@ -3,7 +3,7 @@ package domofon.tck
 import java.util.UUID
 
 import akka.actor.ActorSystem
-import akka.http.scaladsl.model.{HttpResponse, StatusCodes, MediaTypes}
+import akka.http.scaladsl.model.{HttpRequest, HttpResponse, StatusCodes, MediaTypes}
 import akka.http.scaladsl.model.headers.Accept
 import akka.http.scaladsl.server._
 import akka.http.scaladsl.testkit.{RouteTestTimeout, ScalatestRouteTest}
@@ -14,6 +14,8 @@ import org.scalatest.{FunSpec, Matchers}
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
+import scala.util.Try
+import spray.json._
 
 trait BaseTckTest extends FunSpec with Matchers with ScalatestRouteTest {
 
@@ -29,16 +31,77 @@ trait BaseTckTest extends FunSpec with Matchers with ScalatestRouteTest {
 
   def contactRequest() = PostContact("John Smith", "Company Ltd.", "email@domain.pl", "+48123321123")
 
+  def maxSampleLength = 1000
+
   def postContactRequest(cr: PostContact = contactRequest()): UUID = {
     import DomofonMarshalling._
     import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
     import spray.json._
     var uuid: UUID = nonExistentUuid
-    val ret = Post("/contacts", cr.toJson) ~> acceptPlain ~> domofonRoute ~> check {
+    val ret = Post("/contacts", cr.toJson) ~> acceptPlain ~~> {
       status shouldBe StatusCodes.OK
       uuid = UUID.fromString(responseAs[String])
     }
     uuid
+  }
+
+  def formatedEntityString(content: String): String = {
+    Try(content.parseJson.prettyPrint).getOrElse {
+      val len = content.length
+      if (len + 3 > maxSampleLength) {
+        content.take(maxSampleLength) + "..."
+      } else {
+        content
+      }
+    }
+  }
+
+  def requestAsString(request: HttpRequest): String = {
+    val content = Await.result(request.entity.dataBytes.runReduce(_ ++ _), Duration.Inf).utf8String
+
+    s"""
+       |${request.method.value} ${request.uri}
+       |${request.headers.mkString("\n  ")}
+       |Content-type: ${request.entity.contentType}
+       |
+       |
+       |${formatedEntityString(content)}""".stripMargin
+  }
+
+  def responseAsString(response: HttpResponse): String = {
+    val content = Await.result(response.entity.dataBytes.runReduce(_ ++ _), Duration.Inf).utf8String
+    s"""
+       |${response.status.value}
+       |${response.headers.mkString("\n  ")}
+       |Content-type: ${response.entity.contentType}
+       |
+       |
+       |${formatedEntityString(content)}""".stripMargin
+  }
+
+  implicit class PimpedRequest(request: HttpRequest) {
+    /**
+     * Shortcut to run request using domofonRoute
+     *
+     * Prints nicely Request and Response in case of error
+     */
+    def ~~>[T](body: => T) = request ~> domofonRoute ~> check {
+      try {
+        val a = body
+        a
+      } catch {
+        case t: Throwable =>
+          info("  Request was:")
+          info("/--------------------------------------\\")
+          info(requestAsString(request))
+          info("\\--------------------------------------/")
+          info("  Response was:")
+          info("/--------------------------------------\\")
+          info(responseAsString(response))
+          info("\\--------------------------------------/")
+          throw t
+      }
+    }
   }
 
 }
