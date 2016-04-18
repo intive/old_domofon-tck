c170330a06a6444aa279280bfd7952393a0c78ac
Patryk Koryzna <patryk.koryzna@blstream.com>
2016-04-18 14:58:50 +0200

    make message a simple String

--- akka-http-mock/src/main/scala/domofon/mock/akka/Validators.scala
+++ akka-http-mock/src/main/scala/domofon/mock/akka/Validators.scala
@@ -17,42 +17,41 @@ import scala.util.{Failure, Success, Try}
 object Validators {
   type Error = (FieldName, Message)
   type FieldName = String
-  type Message = Option[String]
+  type Message = String
 
   def field[A, B](fieldName: FieldName)(validation: A => ValidatedNel[Message, B]): A => ValidatedNel[Error, B] = { value =>
     validation(value).leftMap(Functor[NonEmptyList].map(_)(msg => (fieldName, msg)))
   }
 
   def nonEmpty: String => ValidatedNel[Message, String] = { s =>
-    if(s.nonEmpty) Valid(s)
-    else Invalid(NonEmptyList(Some(s"string cannot be empty")))
+    if (s.nonEmpty) Valid(s)
+    else Invalid(NonEmptyList(s"string cannot be empty"))
   }
 
   def validEmail: String => ValidatedNel[Message, String] = { email =>
-    if(EmailAddress.isValid(email))
+    if (EmailAddress.isValid(email))
       Valid(email)
     else
-      Invalid(NonEmptyList(Some(s"email $email is invalid")))
+      Invalid(NonEmptyList(s"email $email is invalid"))
   }
 
   def validDate(date: String): ValidatedNel[Message, ZonedDateTime] = {
     Try(java.time.ZonedDateTime.parse(date)) match {
       case Success(zdt) => Valid(zdt)
-      case Failure(_) => Invalid(NonEmptyList(Some(s"date $date is invalid")))
+      case Failure(_)   => Invalid(NonEmptyList(s"date $date is invalid"))
     }
   }
 
-  def validDateRange(fromO: Option[LocalDate], tillO: Option[LocalDate]):
-  ValidatedNel[Message, Option[(LocalDate, LocalDate)]] = {
+  def validDateRange(fromO: Option[LocalDate], tillO: Option[LocalDate]): ValidatedNel[Message, Option[(LocalDate, LocalDate)]] = {
     (fromO, tillO) match {
       case (Some(from), Some(till)) if from.isBefore(till) || from.isEqual(till) =>
         Valid(Some(from, till))
       case (None, None) =>
         Valid(None)
       case (Some(_), Some(_)) =>
-        Invalid(NonEmptyList(Some("\"from\" date is after \"till\" date")))
+        Invalid(NonEmptyList("\"from\" date is after \"till\" date"))
       case _ =>
-        Invalid(NonEmptyList(Some("either both dates must be defined or none")))
+        Invalid(NonEmptyList("either both dates must be defined or none"))
     }
   }
 }
@@ -65,7 +64,6 @@ object ContactRequestValidator {
   def apply(cr: ContactRequest): ValidatedNel[Error, ContactRequest] = {
     (field("email")(validEmail)(cr.notifyEmail) |@|
       field("name")(nonEmpty)(cr.name) |@|
-      field("from")(validDateRange _ tupled)((cr.fromDate, cr.tillDate))
-      ).map { (_, _, _) => cr }
+      field("from")(validDateRange _ tupled)((cr.fromDate, cr.tillDate))).map { (_, _, _) => cr }
   }
 }
